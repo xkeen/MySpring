cd419d5af171c7482ae829d3c3363c15effa604e
keen_xie <keen_xie@foxmail.com>
2017-08-20 12:24:27 +0800

    add bean definition

---
+++ src/main/java/com/xke/myspring/tinyioc/BeanDefinition.java
@@ -0,0 +1,14 @@
+package com.xke.myspring.tinyioc;
+
+public class BeanDefinition {
+	private Object bean;
+    public BeanDefinition(Object bean) {
+		// TODO Auto-generated constructor stub
+		this.bean = bean;
+	}
+    public Object getBean() {
+		return bean;
+	}
+		
+	
+}
